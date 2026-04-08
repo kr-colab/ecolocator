0a71c6b4172dd30f369064ae75214bc97be98e5b
jordannrodriguez <jordanrodriguez48@yahoo.com>
2026-04-08 15:59:05 -0700

    added test_utils and started testing load genotypes

--- ecolocator/tests/test_ecolocator_init.py
+++ ecolocator/tests/test_ecolocator_init.py
@@ -1,2 +1,13 @@
-# TODO
-run first test 
+import pytest
+
+from ecolocator import EcoLocator
+
+def test_ecolocator_init():
+    project_path = "test_project"
+    ecolocator = EcoLocator(project_path=project_path)
+    assert ecolocator.get_project_path() == project_path
+
+
+def test_ecolocator_load_from_yaml():
+    yaml_path = "test_project.yaml"
+    ecolocator = EcoLocator.load_from_yaml(yaml_path=yaml_path)
