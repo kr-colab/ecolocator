d606db84545fde00e66dd16204de52cd778b7dd7
jordannrodriguez <jordanrodriguez48@yahoo.com>
2026-04-08 14:37:22 -0700

    downloaded uv package manager, set the lay of the land, started testing things

---
+++ ecolocator/src/ecolocator/ecolocator_class.py
@@ -0,0 +1,23 @@
+import numpy as np
+import logging
+
+
+class EcoLocator:
+	"""
+	Docstringer describing class
+	"""
+
+	def __init__(self,
+		project_path: str = None,  # path to project directory
+	):
+		self.project_path = project_path
+		logging.info(f"Using project path {self.project_path}")
+
+	def get_project_path(self) -> str:
+		return self.project_path
+
+	@staticmethod	
+	def load_from_yaml(
+		yaml_path: str,
+	) -> EcoLocator:
+		assert False, "NOT IMPLEMENTED YET"
