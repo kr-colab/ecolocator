9819357d640b158f11e53800e1160c8c90376f63
Jordan <jordanrodriguez48@yahoo.com>
2026-04-21 23:43:31 -0700

    editing tests

--- ecolocator/src/ecolocator/utils.py
+++ ecolocator/src/ecolocator/utils.py
@@ -3,17 +3,23 @@ import allel
 import zarr
 import numpy as np
 import pandas as pd
+import logging
 
-def load_genotypes(zarr_path=None, vcf_path=None, matrix_path=None):
+
+def load_genotypes(
+    zarr_path: str = None, 
+    vcf_path: str = None, 
+    matrix_path: str = None,
+) -> (allel.GenotypeArray, np.ndarray):
     if zarr_path is not None:
-        print("reading zarr")
+        logging.info("reading zarr")
         callset = zarr.open_group(zarr_path, mode="r")
         gt = callset["calldata/GT"]
         genotypes = allel.GenotypeArray(gt[:])
         samples = callset["samples"][:]
         positions = callset["variants/POS"]
     elif vcf_path is not None:
-        print("reading VCF")
+        logging.info("reading VCF")
         vcf = allel.read_vcf(vcf_path, log=sys.stderr)
         genotypes = allel.GenotypeArray(vcf["calldata/GT"])
         samples = vcf["samples"]
@@ -47,4 +53,34 @@ def load_genotypes(zarr_path=None, vcf_path=None, matrix_path=None):
         genotypes = allel.HaplotypeArray(np.transpose(hmat)).to_genotypes(ploidy=2)
     else:
         raise ValueError("Must provide one of zarr_path, vcf_path, or matrix_path")
+    logging.info(f"loaded {np.shape(genotypes)} genotypes")
+    logging.info(f"loaded {len(samples)} samples")
     return genotypes, samples
+
+
+# TODO:
+#   - make doc string
+def sort_samples(
+    samples: np.ndarray, 
+    sample_data_path: str, 
+    num_covs: int=1,
+) -> (pd.DataFrame, np.ndarray):
+    """
+    `sample_data_path` points to a tsv with columns "sampleID", "x", "y", and optionally covariate columns "cov1", "cov2", etc.
+    """
+    assert num_covs > 0, "FIXME: can we support 0 covs? (loc pred only)"
+    sample_data = pd.read_csv(sample_data_path, sep="\t")
+    sample_data["sampleID2"] = sample_data["sampleID"]
+    sample_data.set_index("sampleID", inplace=True)
+    samples = samples.astype("str")
+    sample_data = sample_data.reindex(
+        np.array(samples)
+    )  # sort loc table so samples are in same order as vcf samples
+    if not all(
+        [sample_data["sampleID2"].iloc[x] == samples[x] for x in range(len(samples))]
+    ):  # check that all sample names are present
+        raise ValueError("sample ordering failed! Check that sample IDs match the VCF.")
+    locs = np.array(sample_data[["x", "y"] + [f"cov{i+1}" for i in range(num_covs)]])
+    logging.debug(f"Input data:\n{locs}")
+    logging.info(f"sorted samples and covariates for {len(samples)} samples")
+    return sample_data, locs
